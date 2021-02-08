fe26780c8d01787d94847588697e2f8503467fef
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 19:52:58 +0100

    Add class for favorite shows

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -1,15 +1,14 @@
 'use strict';
 const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
 const noImageUrlText = 'Image not found';
-const noImageUrl = `https://via.placeholder.com/210x295/ffffff/666666/?text=${noImageUrlText}`;
-// const noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
+let noImageUrl = `https://via.placeholder.com/210x295/ffffff/666666/?text=${noImageUrlText}`;
+// let noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
 const formElement = document.querySelector('.js-form');
 const searchInputElement = document.querySelector('.js-search-input');
 const searchBtnElement = document.querySelector('.js-search-btn');
-const listElement = document.querySelector('.js-list');
 
-let series = [];
-// let favorites = [];
+let shows = [];
+// let favoriteShows = [];
 
 // local storage
 
@@ -26,31 +25,52 @@ function getDataFromApi() {
   fetch(apiUrl + search)
     .then((response) => response.json())
     .then((data) => {
-      series = data;
-      paintCards();
+      shows = data;
+      paintSearchCards();
     });
 }
 
 searchBtnElement.addEventListener('click', getDataFromApi);
 
 // paint card:
-function paintCards() {
+function paintSearchCards() {
   let htmlCode;
-
-  for (const serie of series) {
-    htmlCode += `<li class="js-list-element id="${serie.show.id}>`;
-    htmlCode += `<h2 class="page__card--title">${serie.show.name}</h2>`;
-    if (serie.show.image === null) {
-      htmlCode += `<img class="js-image page__card--img" src="${noImageUrl}" alt="${serie.show.name}" />`;
+  for (const show of shows) {
+    // paint HTML code
+    htmlCode += `<li class="js-list-element" id="${show.show.id}">`;
+    htmlCode += `<h2 class="page__card--title">${show.show.name}</h2>`;
+    if (show.show.image === null) {
+      htmlCode += `<img class="js-image page__card--img" src="${noImageUrl}" alt="${show.show.name}" />`;
     } else {
-      htmlCode += `<img class="js-image page__card--img" src="${serie.show.image.medium}" alt="${serie.show.name}" />`;
+      htmlCode += `<img class="js-image page__card--img" src="${show.show.image.medium}" alt="${show.show.name}" />`;
     }
     htmlCode += '</li>';
   }
+  const listElement = document.querySelector('.js-list');
   listElement.innerHTML = htmlCode;
   // listen to event after painting
   listenShowEvents();
 }
+
+/*function paintFavoriteCards() {
+  let htmlCode;
+  htmlCode += '<ul class="main__list js-list-favorites">';
+  for (const show of shows) {
+    // paint HTML code
+    htmlCode += `<li class="js-list-element-favorite" id="${show.show.id}">`;
+    htmlCode += `<h2 class="page__card--title">${show.show.name}</h2>`;
+    if (show.show.image === null) {
+      htmlCode += `<img class="js-image page__card--img" src="${noImageUrlFavorite}" alt="${show.show.name}" />`;
+    } else {
+      htmlCode += `<img class="js-image page__card--img" src="${show.show.image.medium}" alt="${show.show.name}" />`;
+    }
+    htmlCode += '</li>';
+  }
+  htmlCode += '</ul>';
+
+  const listFavoriteElement = document.querySelector('.js-favorite-shows');
+  listFavoriteElement.innerHTML = htmlCode;
+}*/
 
 function listenShowEvents() {
   const cardElements = document.querySelectorAll('.js-list-element');
@@ -60,18 +80,11 @@ function listenShowEvents() {
 }
 
 function handleShow(ev) {
-  console.log(ev.currentTarget);
+  const clickedShow = ev.currentTarget;
+  const clickedShowId = ev.currentTarget.id;
+  console.log(clickedShowId);
+  // esto no hay que hacerlo al final, pero hay que ir tirando para conservar la cabeza...
+  clickedShow.classList.toggle('card--favorite');
 }
-
-// const cardElements = document.querySelectorAll('.js-list-element');
-// console.log(cardElements);
-
-// function handleSelectedCard() {
-//   for (cardElement of cardElements) {
-//     cardElement.classList.toggle('card--favorite');
-//   }
-// }
-
-// listElement.addEventListener('click', handleSelectedCard);
 
 getDataFromApi();
