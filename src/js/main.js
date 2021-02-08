ead72837fb347d3e86e28fd3679eeae8d1288276
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 15:18:43 +0100

    Add first variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+const searchInputElement = document.querySelector('.js-search-input');
+const searchBtnElement = document.querySelector('.js-search-btn');
+
+let series = [];
+let favorites = [];
