9eceaccd985d96b8d5a1bdb07c3fcff98b2271b8
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 16:56:14 +0100

    Clean and fix paintCards function

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -2,19 +2,16 @@
 const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
 const noImageUrlText = 'Image not found';
 const noImageUrl = `https://via.placeholder.com/210x295/ffffff/666666/?text=${noImageUrlText}`;
-const noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
+// const noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
 const formElement = document.querySelector('.js-form');
 const searchInputElement = document.querySelector('.js-search-input');
 const searchBtnElement = document.querySelector('.js-search-btn');
-// const titleElement = document.querySelector('.js-title');
 const listElement = document.querySelector('.js-list');
 
 let series = [];
-
 // let favorites = [];
 
 // prevent submit form
-
 function handleForm(ev) {
   ev.preventDefault();
 }
@@ -38,19 +35,17 @@ searchBtnElement.addEventListener('click', getDataFromApi);
 function paintCards() {
   let htmlCode;
   for (const serie of series) {
-    htmlCode += '    <li class="js-list-element">';
     htmlCode += '<article class="page__card">';
-    htmlCode += `<h2 class="js-title page__card--title">${serie.show.name}</h2>`;
+    htmlCode += '<li class="js-list-element">';
+    htmlCode += `<h2 class="page__card--title">${serie.show.name}</h2>`;
     if (serie.show.image === null) {
       htmlCode += `<img class="js-image page__card--img" src="${noImageUrl}" alt="${serie.show.name}" />`;
     } else {
       htmlCode += `<img class="js-image page__card--img" src="${serie.show.image.medium}" alt="${serie.show.name}" />`;
     }
-
     htmlCode += '</li>';
     htmlCode += '</article>';
   }
-  console.log(listElement);
   listElement.innerHTML = htmlCode;
 }
 
