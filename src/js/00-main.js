3a3ece83373fd15e09f769f7edf5f219f87bd13d
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 22:20:49 +0100

    Add fuction to update favoritesShows array

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -2,13 +2,13 @@
 const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
 const noImageUrlText = 'Image not found';
 let noImageUrl = `https://via.placeholder.com/210x295/ffffff/666666/?text=${noImageUrlText}`;
-// let noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
+let noImageUrlFavorite = `https://via.placeholder.com/125x175/ffffff/666666/?text=${noImageUrlText}`;
 const formElement = document.querySelector('.js-form');
 const searchInputElement = document.querySelector('.js-search-input');
 const searchBtnElement = document.querySelector('.js-search-btn');
 
 let shows = [];
-// let favoriteShows = [];
+let favoriteShows = [];
 
 // local storage
 
@@ -34,7 +34,7 @@ searchBtnElement.addEventListener('click', getDataFromApi);
 
 // paint card:
 function paintSearchCards() {
-  let htmlCode;
+  let htmlCode = '';
   for (const show of shows) {
     // paint HTML code
     htmlCode += `<li class="js-list-element" id="${show.show.id}">`;
@@ -52,17 +52,16 @@ function paintSearchCards() {
   listenShowEvents();
 }
 
-/*function paintFavoriteCards() {
-  let htmlCode;
-  htmlCode += '<ul class="main__list js-list-favorites">';
-  for (const show of shows) {
+function paintFavoriteCards() {
+  let htmlCode = '<ul class="main__list js-list-favorites">';
+  for (const favoriteShow of favoriteShows) {
     // paint HTML code
-    htmlCode += `<li class="js-list-element-favorite" id="${show.show.id}">`;
-    htmlCode += `<h2 class="page__card--title">${show.show.name}</h2>`;
-    if (show.show.image === null) {
-      htmlCode += `<img class="js-image page__card--img" src="${noImageUrlFavorite}" alt="${show.show.name}" />`;
+    htmlCode += `<li class="js-list-element-favorite" id="${favoriteShow.show.id}">`;
+    htmlCode += `<h2 class="page__card--title">${favoriteShow.show.name}</h2>`;
+    if (favoriteShow.show.image === null) {
+      htmlCode += `<img class="js-image page__card--img" src="${noImageUrlFavorite}" alt="${favoriteShow.show.name}" />`;
     } else {
-      htmlCode += `<img class="js-image page__card--img" src="${show.show.image.medium}" alt="${show.show.name}" />`;
+      htmlCode += `<img class="js-image page__card--img" src="${favoriteShow.favoriteShow.image.medium}" alt="${favoriteShow.show.name}" />`;
     }
     htmlCode += '</li>';
   }
@@ -70,7 +69,7 @@ function paintSearchCards() {
 
   const listFavoriteElement = document.querySelector('.js-favorite-shows');
   listFavoriteElement.innerHTML = htmlCode;
-}*/
+}
 
 function listenShowEvents() {
   const cardElements = document.querySelectorAll('.js-list-element');
@@ -80,11 +79,23 @@ function listenShowEvents() {
 }
 
 function handleShow(ev) {
-  const clickedShow = ev.currentTarget;
-  const clickedShowId = ev.currentTarget.id;
+  const clickedShowId = Number(ev.currentTarget.id);
   console.log(clickedShowId);
-  // esto no hay que hacerlo al final, pero hay que ir tirando para conservar la cabeza...
-  clickedShow.classList.toggle('card--favorite');
+  // find if the clicked show is already in the favoriteShows array
+  const favoriteCLickedShow = favoriteShows.find((favShow) => favShow.show.id === clickedShowId);
+  if (favoriteCLickedShow === undefined) {
+    // add to favoriteShows[] the shows with the clicked ID.
+    const clickedShow = shows.find((tvShow) => clickedShowId === tvShow.show.id);
+    favoriteShows.push(clickedShow);
+    console.log('No está, lo añado');
+  } else {
+    const filteredFavorites = favoriteShows.filter((favShow) => favShow.show.id !== clickedShowId);
+    favoriteShows = filteredFavorites;
+    console.log('Sí, está, lo quito');
+  }
+  console.log(favoriteCLickedShow);
+
+  console.log(favoriteShows);
 }
 
 getDataFromApi();
