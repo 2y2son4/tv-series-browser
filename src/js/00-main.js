43e50f97906ae128bc31d66c9912f91fd66ff33b
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 17:40:59 +0100

    Add id's to li elements

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -11,6 +11,8 @@ const listElement = document.querySelector('.js-list');
 let series = [];
 // let favorites = [];
 
+// local storage
+
 // prevent submit form
 function handleForm(ev) {
   ev.preventDefault();
@@ -34,9 +36,9 @@ searchBtnElement.addEventListener('click', getDataFromApi);
 // paint card:
 function paintCards() {
   let htmlCode;
+
   for (const serie of series) {
-    htmlCode += '<article class="page__card">';
-    htmlCode += '<li class="js-list-element">';
+    htmlCode += `<li class="js-list-element id="${serie.show.id}>`;
     htmlCode += `<h2 class="page__card--title">${serie.show.name}</h2>`;
     if (serie.show.image === null) {
       htmlCode += `<img class="js-image page__card--img" src="${noImageUrl}" alt="${serie.show.name}" />`;
@@ -44,9 +46,32 @@ function paintCards() {
       htmlCode += `<img class="js-image page__card--img" src="${serie.show.image.medium}" alt="${serie.show.name}" />`;
     }
     htmlCode += '</li>';
-    htmlCode += '</article>';
   }
   listElement.innerHTML = htmlCode;
+  // listen to event after painting
+  listenShowEvents();
 }
+
+function listenShowEvents() {
+  const cardElements = document.querySelectorAll('.js-list-element');
+  for (const cardElement of cardElements) {
+    cardElement.addEventListener('click', handleShow);
+  }
+}
+
+function handleShow(ev) {
+  console.log(ev.currentTarget);
+}
+
+// const cardElements = document.querySelectorAll('.js-list-element');
+// console.log(cardElements);
+
+// function handleSelectedCard() {
+//   for (cardElement of cardElements) {
+//     cardElement.classList.toggle('card--favorite');
+//   }
+// }
+
+// listElement.addEventListener('click', handleSelectedCard);
 
 getDataFromApi();
