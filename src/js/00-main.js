cdbefd1fe58b16087c72c76cf7070c0c3e22d4b9
2y2son4 <adrianadelteso@gmail.com>
2021-02-08 16:24:18 +0100

    Add function paintCards

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -3,7 +3,7 @@ const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
 const searchInputElement = document.querySelector('.js-search-input');
 // const searchBtnElement = document.querySelector('.js-search-btn');
 // const titleElement = document.querySelector('.js-title');
-const listElement = document.querySelector('.js-list-element');
+const listElement = document.querySelector('.js-list');
 
 let series = [];
 
@@ -14,19 +14,23 @@ function getDataFromApi() {
   fetch(apiUrl + search)
     .then((response) => response.json())
     .then((data) => {
-      let htmlCode;
       series = data;
-      for (const serie of series) {
-        htmlCode += '<article class="page__card">';
-        htmlCode += `<h2 class="js-title page__card--title">${serie.show.name}</h2>`;
-        htmlCode += `<img class="js-image page__card--img" src="${serie.show.image.medium}" alt="${serie.show.name}" />`;
-        htmlCode += '</article>';
-      }
-      listElement.innerHTML = htmlCode;
+      paintCards();
     });
 }
 
-// function paintCards() {
-// }
+function paintCards() {
+  let htmlCode;
+  for (const serie of series) {
+    htmlCode += '    <li class="js-list-element">';
+    htmlCode += '<article class="page__card">';
+    htmlCode += `<h2 class="js-title page__card--title">${serie.show.name}</h2>`;
+    htmlCode += `<img class="js-image page__card--img" src="${serie.show.image.medium}" alt="${serie.show.name}" />`;
+    htmlCode += '</li>';
+    htmlCode += '</article>';
+  }
+  console.log(listElement);
+  listElement.innerHTML = htmlCode;
+}
 
 getDataFromApi();
